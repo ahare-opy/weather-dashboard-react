2cc3697a8fdb8c67c63f3ee29852d6265515d7b9
TanvirYeasinOpy <tanvir016opy@gmail.com>
2023-11-17 22:47:47 +0600

    Applied all the hookers and api.

---
+++ src/hooks/useCoordinations.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const useCoordinations = () => {
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  const [loadingLocation, setLoadingLocation] = useState(false);
+
+  const findCoordinates = () => {
+    if (navigator.geolocation) {
+      setLoadingLocation(true);
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          setLat(pos.coords.latitude);
+          setLong(pos.coords.longitude);
+        },
+        (positionError) => {
+          setLat("41.390205");
+          setLong("2.154007");
+          console.log(poistionError);
+        }
+      );
+      setLoadingLocation(false);
+    } else {
+      console.log("It's not supported by this browser.");
+    }
+  };
+
+  const updateLocation = (lat, long) => {
+    setLat(lat);
+    setLong(long);
+  };
+
+  return [{ lat, long }, loadingLocation, findCoordinates, updateLocation];
+};
