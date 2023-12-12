e139756008b3a51e8bb9a637875e19d76da433f3
Tanvir Yeasin Opy <tanvir016opy@gmail.com>
2023-12-13 01:11:37 +0600

    removed api keys

--- src/api.js
+++ src/api.js
@@ -3,13 +3,13 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'b56c15afc3mshf62153c24dd89a3p13fcc9jsn1ed7b65d6562',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const WEATHER_API_KEY = 'ac258c45dcd8b616bd887e19d52a4060';
+export const WEATHER_API_KEY = '';
 
-export const UNSPLASH_API_KEY = 'uKqp_P3fKiIHhAcLNUTvDXGnzEZAeO6oM_eI6ws_ZW8'
+export const UNSPLASH_API_KEY = ''
